8cd171c327c4647fc19ec39bc8cf43d46148029d
Tigran <harutyunyan.tigran1975@gmail.com>
2024-12-10 14:45:45 +0400

    created Document component that holds Navbar, Toolbar and Editor components

---
+++ src/components/document/Document.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import Editor from "@/components/document/Editor.vue";
+import Navbar from "@/components/document/Navbar.vue";
+import Toolbar from "@/components/document/Toolbar/Toolbar.vue";
+import { useGetDocumentById } from "@/api/useGetDocumentById";
+import { Id } from "../../../convex/_generated/dataModel";
+import { useEditorStore } from "@/store/useEditorStore";
+import { watch } from "vue";
+
+interface documentProps {
+  id: Id<"documents">;
+}
+const props = defineProps<documentProps>();
+
+const { data: document } = useGetDocumentById(props.id);
+const { setEditorMargins } = useEditorStore();
+
+watch(
+  () => document?.value,
+  (newData) => {
+    if (newData?.setting) {
+      const settings = JSON.parse(newData?.setting);
+      if (Array.isArray(settings.margins)) {
+        setEditorMargins(settings.margins);
+      }
+    }
+  },
+  {
+    immediate: true,
+  }
+);
+</script>
+
+<template>
+  <div class="min-h-screen bg-[#FAFBFD]" v-if="document">
+    <div
+      class="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#FAFBFD] print:hidden"
+    >
+      <Navbar :data="document" />
+      <Toolbar />
+    </div>
+    <div class="pt-[114px] print:pt-0">
+      <Editor :initialContent="document?.initialContent" />
+    </div>
+  </div>
+</template>
