ed56c56a056053f9dbe6516701109c4413a2cc5a
Tigran <harutyunyan.tigran1975@gmail.com>
2024-12-10 14:51:30 +0400

    created auth Views

---
+++ src/views/SignInView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { SignIn } from "vue-clerk";
+import AuthLayout from "@/layouts/AuthLayout.vue";
+</script>
+
+<template>
+  <AuthLayout>
+    <SignIn sign-up-url="/sign-up" />
+  </AuthLayout>
+</template>
---
+++ src/views/SignUpView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { SignUp } from "vue-clerk";
+import AuthLayout from "@/layouts/AuthLayout.vue";
+</script>
+
+<template>
+  <AuthLayout>
+    <SignUp sign-in-url="/sign-in" />
+  </AuthLayout>
+</template>
